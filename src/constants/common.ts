cfa27ee1ef04f786075b710929b11ddf9bfd6bc9
junayonmiguel <junayonmiguel@gmail.com>
2024-09-06 08:16:11 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Miguel H. Junayon';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Skilled video editor with a strong background in software development, responsible for editing, testing, and producing high-quality videos and software projects. Possess expertise in multimedia tools, programming languages, and software development methodologies.';
+export const ABOUT_TEXT_2 =
+  'Currently focused on video editing, while utilizing past experience in web applications and development. Work closely with clients to create scalable and visually appealing content that meets business and creative objectives.';
+export const ABOUT_TEXT_3 =
+  'Versatile professional proficient in video editing and software development, with the ability to produce compelling multimedia projects from start to finish. Possess a strong understanding of various editing tools, programming languages, and design principles.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Video Editor with extensive experience in multimedia editing, alongside a background in software development. Skilled in video production and creating web-based applications using modern technologies.';
+export const RESUME_TEXT_2 =
+  'Video Editor with a robust background in software development, proficient in both video production and programming tools/frameworks, ensuring high-quality output for various clients and projects.';
